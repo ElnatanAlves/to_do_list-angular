8f84b707bd762d6bbfbba1c0c4783b0d2d18f9f2
Elnatan Alves <156375539+ElnatanAlves@users.noreply.github.com>
2025-09-15 23:48:21 -0300

    v0.1 -ToDoList

---
+++ ToDoListAngular/src/app/app.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed: boolean;
+}
+ 
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, FormsModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  todoList :TodoItem[]= [];
+  newtask : string='';
+}
